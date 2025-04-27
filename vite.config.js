66ac53d543867ef2d8a06d54bf832a9ec71e52a2
wl39 <iwg6852@gmail.com>
2025-04-27 04:44:18 +0000

    Hosts Added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+	allowedHosts: ['wl39.ddns.net'],
     port: 3000, // Keep CRA port
   },
 });
